a1d8ebbfe5902b074757d7fc42387a28463f3cb3
suraj192 <surajsharma1921@gmail.com>
2022-02-08 13:58:32 +0200

    event-listner prac

--- practice_javascript/Practice_lessons070222/DOM_inter/dom22/index.js
+++ practice_javascript/Practice_lessons070222/DOM_inter/dom22/index.js
@@ -1,6 +1,7 @@
 const initClickGreeting = () => {
-    console.log("Welcome!");
-}
-
+  console.log("Welcome!");
+};
+const button = document.querySelector("#my-button");
+button.addEventListener("click", initClickGreeting);
 // Sample usage - do not modify
 initClickGreeting();
