00ca1b2add7a36b6c4da22c6f7529575fd05c1d2
suraj192 <surajsharma1921@gmail.com>
2022-02-14 14:23:53 +0200

    classes practice

--- practice_javascript/Practice_lessons11402/classes/class3.js
+++ practice_javascript/Practice_lessons11402/classes/class3.js
@@ -1,6 +1,13 @@
-'use strict';
+"use strict";
 
 /**
 Write the Recipe class such that it automatically console logs the string: New recipe created whenever
 we create a new instance of that class.
 **/
+class Recipe {
+  constructor() {
+    console.log("New receipe created");
+  }
+}
+let receipe1 = new Recipe();
+let receipe2 = new Recipe();
